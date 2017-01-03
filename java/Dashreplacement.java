bde5545c14f7c665c93a6b46beef77d6c088f8a6
wiehsingli <wiehsing@gmail.com>
2017-01-02 23:19:37 -0800

    dash replacement

---
+++ java/Dashreplacement.java
@@ -0,0 +1,30 @@
+/**
+ * Created by Wie Hsing Li on 12/22/2016.
+ */
+public class Dashreplacement {
+
+    public String solution(String S, int K) {
+//        // write your code in Java SE 8
+        String string = noDashes(S);
+
+        StringBuilder str = new StringBuilder(string);
+        //Start off at the first chunk of characters
+        int index = str.length() - K;
+
+        while( index > 0 ){
+            str.insert(index, "-");
+            index = index - K;
+        }
+        return new String(str.toString().toUpperCase());
+    }
+
+    public String noDashes(String S){
+
+        StringBuilder string = new StringBuilder();
+        String[] stringarr = S.split("-");
+        for (String str : stringarr) {
+            string.append(str);
+        }
+        return string.toString();
+    }
+}
